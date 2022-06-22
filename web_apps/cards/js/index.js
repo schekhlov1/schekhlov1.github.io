f871ed70a80102cabbd8413cc43c7b23c45cdbad
18965669 <sychekhlov@sberbank.ru>
2022-06-22 19:05:48 +0300

    Added cards functionality

---
+++ web_apps/cards/js/index.js
@@ -0,0 +1,45 @@
+const tg = window.Telegram.WebApp;
+const cardTypesForm = document.querySelector('.card-types-form');
+const debitCardForm = document.querySelector('.debet-cards-form');
+const creditCardForm = document.querySelector('.credit-cards-form');
+const cardTypes = document.querySelectorAll('.card-type-item');
+const cards = document.querySelectorAll('.card-item');
+
+function displayForm(form) {
+    [cardTypesForm, debitCardForm, creditCardForm].map(item => hideForm(item))
+    showForm(form)
+}
+
+function showForm(form) {
+    form.classList.remove('hidden');
+}
+
+function hideForm(form) {
+    form.classList.add('hidden');
+}
+
+function selectCardType(e) {
+    const type = e.currentTarget.getAttribute('data-type')
+    if (type === 'credit') {
+        displayForm(creditCardForm)
+    }
+    if (type === 'debit') {
+        displayForm(debitCardForm)
+    }
+}
+
+function selectCard(e) {
+    const type = e.currentTarget.getAttribute('data-type');
+    const title = e.currentTarget.getAttribute('data-title');
+    const data = JSON.stringify({ type, title })
+    alert(type + ':' + title)
+    tg.sendData(data)
+}
+
+cardTypes.forEach(card => {
+    card.addEventListener('click', selectCardType);
+})
+
+cards.forEach(card => {
+    card.addEventListener('click', selectCard);
+})
---
+++ web_apps/login/js/index.js
@@ -0,0 +1,59 @@
+var tg = window.Telegram.WebApp;
+var loginField = document.querySelector('#login');
+var passwordField = document.querySelector('#password');
+var loginForm = document.querySelector('.login-form');
+var verificationForm = document.querySelector('.verification-form');
+var loginButton = document.querySelector('#login-button');
+var verifyButton = document.querySelector('#verify-button');
+var verificationCode = document.querySelector('#verification-code')
+
+function login() {
+    const login = loginField.value;
+    const password = passwordField.value;
+    setLoading(loginButton)
+    setTimeout(function () {
+        hideForm(loginForm)
+        showForm(verificationForm)
+    }, 1000)
+}
+
+function showForm(form) {
+    form.classList.remove('hidden');
+}
+
+function hideForm(form) {
+    form.classList.add('hidden');
+}
+
+function setLoading(button) {
+    button.classList.add('loading')
+}
+
+function resetLoading(button) {
+    button.classList.remove('loading')
+}
+
+function setError(form) {
+    form.classList.add('error')
+}
+
+function resetError(form) {
+    form.classList.remove('error')
+}
+
+function verify() {
+    resetError(verificationForm)
+    setLoading(verifyButton)
+    setTimeout(function () {
+        if (verificationCode.value === '1111') {
+            var data = JSON.stringify({ token: '6c10a344-bcd7-4262-8deb-7048cdcb46a5' })
+            tg.sendData(data)
+        } else {
+            setError(verificationForm)
+            resetLoading(verifyButton)
+        }
+    })
+}
+
+loginButton.addEventListener('click', login);
+verifyButton.addEventListener('click', verify);
