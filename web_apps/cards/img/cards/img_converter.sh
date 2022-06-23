c45241a83f70eacf805f2a5f8bede1e20255ad87
18965669 <sychekhlov@sberbank.ru>
2022-06-23 11:14:48 +0300

    Image optimization

---
+++ web_apps/cards/img/cards/img_converter.sh
@@ -0,0 +1,14 @@
+svgexport credit.svg credit.png
+svgexport credit.svg credit@2.png 2x
+
+svgexport debit.svg debit.png
+svgexport debit.svg debit@2.png 2x
+
+svgexport mastercard_classic.svg mastercard_classic.png
+svgexport mastercard_classic.svg mastercard_classic@2.png 2x
+
+svgexport virtual_mir.svg virtual_mir.png
+svgexport virtual_mir.svg virtual_mir@2.png 2x
+
+svgexport visa_classic.svg visa_classic.png
+svgexport visa_classic.svg visa_classic@2.png 2x
